ed2682c1adf14852ae3d77edc7731e93d1d4c596
ARCXIV <smashdash2003@gmail.com>
2026-04-17 22:11:13 +0200

    Add files via upload

--- gamemodes/zcity/gamemode/modes/tdm_ww2/sh_ww2.lua
+++ gamemodes/zcity/gamemode/modes/tdm_ww2/sh_ww2.lua
@@ -10,7 +10,7 @@ MODE.SiteInsideText = "On objective!"
 MODE.HostageZoneLabel = "EXTRACTION ZONE"
 MODE.SkipSpawnAppearance = true
 MODE.ThemeMusicFile = "tdm_ww2_theme.mp3"
-MODE.ThemeMusicVolume = 0.35
+MODE.ThemeMusicVolume = 0.60
 MODE.BuyMenuTheme = {
     Background = Color(0, 18, 28, 165),
     InnerBackground = Color(3, 30, 44, 145),
@@ -50,7 +50,7 @@ local function AddItemToBUY(ItemName, Type, ItemClass, Price, Category, Attachme
 end
 
 -- Pistols
-AddItemToBUY("Colt Python", "Weapon", "weapon_python", 650, "Pistols", {}, nil, TEAM_GERMAN)
+AddItemToBUY("Walther P38", "Weapon", "weapon_p38", 450, "Pistols", {}, nil, TEAM_GERMAN)
 AddItemToBUY("Colt M1911", "Weapon", "weapon_m1911", 400, "Pistols", {}, nil, TEAM_AMERICAN)
 AddItemToBUY("Walther PPK", "Weapon", "weapon_ppk", 350, "Pistols", {}, nil, TEAM_AMERICAN)
 AddItemToBUY("Mauser M712", "Weapon", "weapon_m712", 700, "Pistols", {}, nil, TEAM_GERMAN)
@@ -67,7 +67,10 @@ AddItemToBUY("M14", "Weapon", "weapon_m14", 3200, "Assault Rifles", {}, nil, TEA
 
 -- Submachine guns
 AddItemToBUY("MP40", "Weapon", "weapon_mp40", 1500, "Submachine Guns", {}, nil, TEAM_GERMAN)
+AddItemToBUY("Suomi KP/-31", "Weapon", "weapon_kp31", 1700, "Submachine Guns", {}, nil, TEAM_GERMAN)
 AddItemToBUY("MAT-49", "Weapon", "weapon_mat49", 1500, "Submachine Guns", {}, nil, TEAM_AMERICAN)
+AddItemToBUY("PPSh-41", "Weapon", "weapon_ppsh", 1600, "Submachine Guns", {}, nil, TEAM_AMERICAN)
+AddItemToBUY("Voere SAM-180", "Weapon", "weapon_sam180", 1800, "Submachine Guns", {}, nil, TEAM_AMERICAN)
 AddItemToBUY("Thompson", "Weapon", "weapon_thompson", 1900, "Submachine Guns", {}, nil, TEAM_AMERICAN)
 AddItemToBUY("MPL", "Weapon", "weapon_mpl", 1600, "Submachine Guns", {}, nil, TEAM_AMERICAN)
 AddItemToBUY("M3 Grease Gun", "Weapon", "weapon_m3greasegun", 1300, "Submachine Guns", {}, nil, TEAM_AMERICAN)
@@ -79,7 +82,15 @@ AddItemToBUY("MG 34", "Weapon", "weapon_mg34", 4500, "MGs", {}, nil, TEAM_GERMAN
 -- Equipment
 AddItemToBUY("Stahlhelm", "Armor", "ent_armor_helmet1", 350, "Equipment", {}, nil, TEAM_GERMAN)
 AddItemToBUY("Field Helmet", "Armor", "ent_armor_helmet7", 350, "Equipment", {}, nil, TEAM_AMERICAN)
-AddItemToBUY("Field Vest", "Armor", "ent_armor_vest1", 1000, "Equipment", {}, nil, TEAM_AMERICAN)
+
+-- Melee
+AddItemToBUY("Buck 120 General", "Weapon", "weapon_buck200knife", 300, "Melee", {}, nil, TEAM_AMERICAN)
+AddItemToBUY("Spaten", "Weapon", "weapon_hg_crovel", 300, "Melee", {}, nil, TEAM_GERMAN)
+
+-- Medical
+AddItemToBUY("Bandage", "Weapon", "weapon_bandage_sh", 200, "Medical", {})
+AddItemToBUY("Morphine", "Weapon", "weapon_morphine", 1000, "Medical", {})
+AddItemToBUY("Tourniquet", "Weapon", "weapon_tourniquet", 150, "Medical", {})
 
 -- Grenades
 AddItemToBUY("Type 59 Grenade", "Weapon", "weapon_hg_type59_tpik", 450, "Grenades", {}, nil, TEAM_GERMAN)
@@ -92,6 +103,7 @@ AddItemToBUY("7.65x17mm (30)", "Ammo", "ent_ammo_7.65x17mm", 75, "Ammo", {}, 30)
 AddItemToBUY("7.62x25mm (30)", "Ammo", "ent_ammo_7.62x25mm", 90, "Ammo", {}, 30)
 AddItemToBUY(".45 ACP (30)", "Ammo", "ent_ammo_.45acp", 75, "Ammo", {}, 30)
 AddItemToBUY(".357 Magnum (20)", "Ammo", "ent_ammo_.357magnum", 75, "Ammo", {}, 20)
+AddItemToBUY(".22 Long Rifle (60)", "Ammo", "ent_ammo_.22longrifle", 50, "Ammo", {}, 60)
 AddItemToBUY("5.56x45mm (30)", "Ammo", "ent_ammo_5.56x45mm", 100, "Ammo", {}, 30)
 AddItemToBUY("7.62x51mm (20)", "Ammo", "ent_ammo_7.62x51mm", 150, "Ammo", {}, 20)
 AddItemToBUY("7.62x54mm (20)", "Ammo", "ent_ammo_7.62x54mm", 100, "Ammo", {}, 20)
