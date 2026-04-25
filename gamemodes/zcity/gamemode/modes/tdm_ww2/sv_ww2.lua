9c3c4008e609fcb4a6fbb45c8ae86cca20850dd7
Zorf <47614494+RealZorf@users.noreply.github.com>
2026-04-25 13:49:05 +0300

    Replaced WW2 models

--- gamemodes/zcity/gamemode/modes/tdm_ww2/sv_ww2.lua
+++ gamemodes/zcity/gamemode/modes/tdm_ww2/sv_ww2.lua
@@ -3,24 +3,22 @@ local MODE = MODE
 local TEAM_GERMAN = 0
 local TEAM_AMERICAN = 1
 
-local teamOfficer = {
-    [TEAM_GERMAN] = nil,
-    [TEAM_AMERICAN] = nil,
-}
-
 local teamData = {
     [TEAM_GERMAN] = {
         role = "German Forces",
         color = Color(120, 105, 85),
-        infantry = {
-            "models/germans/infantry/m38_s2_01.mdl",
-            "models/germans/infantry/m38_s2_02.mdl",
-            "models/germans/infantry/m38_s2_03.mdl",
-            "models/germans/infantry/m38_s2_04.mdl",
-            "models/germans/infantry/m38_s2_05.mdl",
-            "models/germans/infantry/m38_s2_06.mdl",
+        models = {
+            -- NEW
+            "models/wehrmacht_male01v1pm.mdl",
+            "models/wehrmacht_male02v1pm.mdl",
+            "models/wehrmacht_male03v1pm.mdl",
+            "models/wehrmacht_male04v1pm.mdl",
+            "models/wehrmacht_male05v1pm.mdl",
+            "models/wehrmacht_male06v1pm.mdl",
+            "models/wehrmacht_male07v1pm.mdl",
+            "models/wehrmacht_male08v1pm.mdl",
+            "models/wehrmacht_male09v1pm.mdl",
         },
-        officer = "models/germans/officer/m38_s1_02.mdl",
         radio = function()
             return math.Round(math.Rand(100, 108), 1)
         end,
@@ -29,97 +27,49 @@ local teamData = {
     [TEAM_AMERICAN] = {
         role = "American Forces",
         color = Color(70, 120, 85),
-        infantry = {
-            "models/americans/infantry/m41_s1_01.mdl",
-            "models/americans/infantry/m41_s1_02.mdl",
-            "models/americans/infantry/m41_s1_03.mdl",
-            "models/americans/infantry/m41_s1_04.mdl",
-            "models/americans/infantry/m41_s1_05.mdl",
-            "models/americans/infantry/m41_s1_06.mdl",
+        models = {
+            -- NEW
+            "models/normandyusmc1pm.mdl",
+            "models/normandyusmc2pm.mdl",
+            "models/normandyusmc3pm.mdl",
+            "models/normandyusmc4pm.mdl",
+            "models/normandyusmc5pm.mdl",
+            "models/normandyusmc6pm.mdl",
+            "models/normandyusmc7pm.mdl",
+            "models/normandyusmc8pm.mdl",
+            "models/normandyusmc9pm.mdl",
         },
-        officer = "models/americans/officer/m37_s1_01.mdl",
         radio = function()
             return math.Round(math.Rand(88, 95), 1)
         end,
     },
 }
 
+-- 🔥 YOUR ORIGINAL BODYGROUP SYSTEM
 local baseBodygroups = {
     german_infantry = {
-        [0] = 0, -- Soldat
-        [1] = 5, -- headgear
-        [2] = 0, -- helmet decals
-        [3] = 0, -- helmet accessory
-        [4] = 0, -- helmet paints
-        [5] = 0, -- facial features
-        [6] = 0, -- neck award
-        [7] = 0, -- german cross
-        [8] = 0, -- iron cross 1st class
-        [9] = 0, -- iron cross 2nd class
-        [10] = 0, -- tunic
-        [11] = 0, -- belt
-        [12] = 0, -- trousers
-        [13] = 0, -- hands
-        [14] = 0, -- Collar Tabs
-        [15] = 0, -- rank
-        [16] = 0, -- facewear
-        [17] = 0, -- entrenchingtools
-        [18] = 0, -- backpack
-        [19] = 0, -- Binos
-        [20] = 0, -- Kit
-        [21] = 0, -- mapcase
-    },
 
-    german_officer = {
-        [0] = 0, -- Soldat
-        [1] = 2, -- headgear
-        [2] = 0, -- facial features
-        [3] = 0, -- neck award
-        [4] = 0, -- german cross
-        [5] = 0, -- iron cross 1st class
-        [6] = 0, -- iron cross 2nd class
-        [7] = 0, -- tunic
-        [8] = 0, -- aiguilette
-        [9] = 0, -- belt
-        [10] = 0, -- sidestrap
-        [11] = 0, -- trousers
-        [12] = 0, -- hands
-        [13] = 0, -- Collar Tabs
-        [14] = 0, -- sabre
-        [15] = 0, -- rank
-        [16] = 0, -- facewear
-        [17] = 0, -- Binos
-        [18] = 0, -- holster
-        [19] = 0, -- mapcase
+        [0] = 0, -- wehrmacht_male01
+        [1] = 0, -- hands
+        [2] = 3, -- Helmet
+        [3] = 0, -- Coat
+        [4] = 0, -- goggles
+        [5] = 0, -- shemagh
+        [6] = 0, -- Grenades
+        [7] = 0, -- Uniforminsignia
+        [8] = 0, -- Scarf
+        [9] = 0, -- Gasmask
+        [10] = 0, -- Rank
+        [11] = 0, -- Gear
     },
 
     american_infantry = {
-        [0] = 0, -- Soldat
-        [1] = 4, -- headgear
-        [2] = 0, -- helmet_decal
-        [3] = 0, -- tunic
-        [4] = 0, -- trousers
-        [5] = 0, -- hands
-        [6] = 0, -- facewear
-        [7] = 0, -- flashlight
-        [8] = 0, -- watch
-        [9] = 0, -- kit
-        [10] = 0, -- extragear
-        [11] = 0, -- backpack
-    },
-
-    american_officer = {
-        [0] = 0, -- Soldat
-        [1] = 4, -- headgear
-        [2] = 0, -- rank
-        [3] = 0, -- tunic
-        [4] = 0, -- trousers
-        [5] = 0, -- hands
-        [6] = 0, -- facewear
-        [7] = 0, -- flashlight
-        [8] = 0, -- watch
-        [9] = 0, -- kit
-        [10] = 0, -- backpack
+        [0] = 0, -- soldier
+        [1] = 0, -- hand
+        [2] = 0, -- body
+        [3] = 4, -- Helmet
+        [4] = 0, -- Gear
+        [5] = 1, -- Beach_Gear
     }
 }
 
@@ -131,27 +81,13 @@ local function IsWW2Round()
     return mode and mode.name == "ww2"
 end
 
-local function AssignOfficers()
-    for teamID, _ in pairs(teamData) do
-        local candidates = {}
-
-        for _, ply in player.Iterator() do
-            if ply:Team() == teamID and ply:Alive() then
-                table.insert(candidates, ply)
-            end
-        end
-
-        teamOfficer[teamID] = (#candidates > 0) and table.Random(candidates) or nil
-    end
-end
-
 local function GetTeamInfo(ply)
     return teamData[ply:Team()] or teamData[TEAM_GERMAN]
 end
 
-local function GetInfantryModel(ply, info)
-    local list = info.infantry
-    if not list or #list == 0 then return info.officer end
+local function GetPlayerModel(ply, info)
+    local list = info.models
+    if not list or #list == 0 then return nil end
 
     local seed = util.CRC(ply:SteamID64() or tostring(ply:EntIndex()))
     local index = (tonumber(seed) % #list) + 1
@@ -159,13 +95,14 @@ local function GetInfantryModel(ply, info)
     return list[index]
 end
 
-local function ApplyBodygroups(ply, model, role)
+local function ApplyBodygroups(ply, model)
     local groups
 
-    if string.find(model, "germans") then
-        groups = (role == "officer") and baseBodygroups.german_officer or baseBodygroups.german_infantry
-    elseif string.find(model, "americans") then
-        groups = (role == "officer") and baseBodygroups.american_officer or baseBodygroups.american_infantry
+    if string.find(model, "wehrmacht") then
+        groups = baseBodygroups.german_infantry
+
+    elseif string.find(model, "usmc") or string.find(model, "american") then
+        groups = baseBodygroups.american_infantry
     end
 
     if not groups then return end
@@ -178,22 +115,12 @@ end
 local function ApplyTeamModel(ply, info)
     if not IsValid(ply) or not info then return end
 
-    local teamID = ply:Team()
-    local isOfficer = teamOfficer[teamID] == ply
-
-    local model, role
-
-    if isOfficer then
-        model = info.officer
-        role = "officer"
-    else
-        model = GetInfantryModel(ply, info)
-        role = "infantry"
-    end
+    local model = GetPlayerModel(ply, info)
+    if not model then return end
 
     if util.IsValidModel and not util.IsValidModel(model) then
         if not modelWarnings[model] then
-            print("[WW2] Invalid model but applying anyway: " .. model)
+            print("[WW2] Model invalid, forcing anyway: " .. model)
             modelWarnings[model] = true
         end
     end
@@ -204,8 +131,7 @@ local function ApplyTeamModel(ply, info)
 
     ply:SetModel(model)
 
-    -- 🎯 ALWAYS APPLY CORRECT BODYGROUPS
-    ApplyBodygroups(ply, model, role)
+    ApplyBodygroups(ply, model)
 
     ply:SetPlayerColor(Vector(1, 1, 1))
     ply:SetNetVar("Accessories", "none")
@@ -239,8 +165,6 @@ end
 
 function MODE:GiveEquipment()
     timer.Simple(0.1, function()
-        AssignOfficers()
-
         for _, ply in player.Iterator() do
             if not ply:Alive() then continue end
 
@@ -289,7 +213,6 @@ hook.Add("PlayerSpawn", "ZB_WW2_ApplyTeamModel", function(ply)
 end)
 
 local nextModelCheck = 0
-
 hook.Add("Think", "ZB_WW2_KeepTeamModels", function()
     if nextModelCheck > CurTime() then return end
     nextModelCheck = CurTime() + 1
@@ -300,16 +223,12 @@ hook.Add("Think", "ZB_WW2_KeepTeamModels", function()
         if not ply:Alive() then continue end
 
         local info = GetTeamInfo(ply)
+        local expectedModel = GetPlayerModel(ply, info)
 
-        local isOfficer = teamOfficer[ply:Team()] == ply
-        local expectedModel = isOfficer and info.officer or GetInfantryModel(ply, info)
-        local role = isOfficer and "officer" or "infantry"
-
-        if ply:GetModel() ~= expectedModel then
+        if expectedModel and ply:GetModel() ~= expectedModel then
             ApplyTeamModel(ply, info)
         else
-            -- 🔧 keep bodygroups synced even if model is correct
-            ApplyBodygroups(ply, expectedModel, role)
+            ApplyBodygroups(ply, expectedModel)
         end
     end
 end)
