551c91ddc3bf5c41915a975f8be69405ce87be6b
Zorf <47614494+RealZorf@users.noreply.github.com>
2026-04-26 15:23:23 +0300

    Updated WW2
    Balanced gun prices and reworked model selection logic

--- gamemodes/zcity/gamemode/modes/tdm_ww2/sh_ww2.lua
+++ gamemodes/zcity/gamemode/modes/tdm_ww2/sh_ww2.lua
@@ -49,69 +49,69 @@ local function AddItemToBUY(ItemName, Type, ItemClass, Price, Category, Attachme
     }
 end
 
--- Pistols
-AddItemToBUY("Walther PPK", "Weapon", "weapon_ppk", 600, "Pistols", {}, nil, TEAM_GERMAN)
-AddItemToBUY("Walther P38", "Weapon", "weapon_p38", 750, "Pistols", {}, nil, TEAM_GERMAN)
-AddItemToBUY("Colt Python", "Weapon", "weapon_python", 950, "Pistols", {} )
-AddItemToBUY("Tokarev TT-33", "Weapon", "weapon_tokarev", 650, "Pistols", {}, nil, TEAM_AMERICAN)
+--Pistols
+AddItemToBUY("Colt Python", "Weapon", "weapon_python", 900, "Pistols", {})
 AddItemToBUY("Colt M1911", "Weapon", "weapon_m1911", 800, "Pistols", {}, nil, TEAM_AMERICAN)
+AddItemToBUY("Walther P38", "Weapon", "weapon_p38", 750, "Pistols", {}, nil, TEAM_GERMAN)
+AddItemToBUY("Tokarev TT-33", "Weapon", "weapon_tokarev", 650, "Pistols", {}, nil, TEAM_AMERICAN)
+AddItemToBUY("Walther PPK", "Weapon", "weapon_ppk", 600, "Pistols", {}, nil, TEAM_GERMAN)
 
--- Submachine guns
+--Submachine guns
+AddItemToBUY("Suomi KP-31 Drum", "Weapon", "weapon_kp31", 4200, "Submachine Guns", {}, nil, TEAM_GERMAN)
+AddItemToBUY("PPSh-41 Drum", "Weapon", "weapon_ppshboss", 4200, "Submachine Guns", {}, nil, TEAM_AMERICAN)
+AddItemToBUY("Thompson M1A1", "Weapon", "weapon_thompson", 3100, "Submachine Guns", {}, nil, TEAM_AMERICAN)
 AddItemToBUY("MP40", "Weapon", "weapon_mp40", 2400, "Submachine Guns", {}, nil, TEAM_GERMAN)
-AddItemToBUY("Suomi KP-31 Drum", "Weapon", "weapon_kp31", 4800, "Submachine Guns", {}, nil, TEAM_GERMAN)
 AddItemToBUY("M3 Grease Gun", "Weapon", "weapon_m3greasegun", 2400, "Submachine Guns", {}, nil, TEAM_AMERICAN)
-AddItemToBUY("Thompson M1A1", "Weapon", "weapon_thompson", 3400, "Submachine Guns", {}, nil, TEAM_AMERICAN)
-AddItemToBUY("PPSh-41 Drum", "Weapon", "weapon_ppshboss", 5000, "Submachine Guns", {}, nil, TEAM_AMERICAN)
 
--- Rifles
+--Rifles
+AddItemToBUY("Gewehr 43", "Weapon", "weapon_gewehr43", 3200, "Rifles", {})
 AddItemToBUY("Karabiner 98k", "Weapon", "weapon_kar98", 2100, "Rifles", {"optic12"}, nil, TEAM_GERMAN)
-AddItemToBUY("Gewehr 43", "Weapon", "weapon_gewehr43", 3800, "Rifles", {} )
 AddItemToBUY("Mosin-Nagant", "Weapon", "weapon_mosin", 2100, "Rifles", {"optic12"}, nil, TEAM_AMERICAN)
 
--- Assault rifles
-AddItemToBUY("FG 42", "Weapon", "weapon_fg42", 5750, "Assault Rifles", {}, nil, TEAM_GERMAN)
-AddItemToBUY("M14", "Weapon", "weapon_m14", 5750, "Assault Rifles", {}, nil, TEAM_AMERICAN)
+--Assault rifles
+AddItemToBUY("FG 42", "Weapon", "weapon_fg42", 4400, "Assault Rifles", {}, nil, TEAM_GERMAN)
+AddItemToBUY("M14", "Weapon", "weapon_m14", 4400, "Assault Rifles", {}, nil, TEAM_AMERICAN)
 
--- Machine guns
-AddItemToBUY("DP-27", "Weapon", "weapon_dp27", 7000, "Machine guns", {}, nil, TEAM_AMERICAN)
-AddItemToBUY("MG 34", "Weapon", "weapon_mg34", 7800, "Machine guns", {}, nil, TEAM_GERMAN)
+--Machine guns
+AddItemToBUY("MG 34", "Weapon", "weapon_mg34", 6800, "Machine guns", {}, nil, TEAM_GERMAN)
+AddItemToBUY("DP-27", "Weapon", "weapon_dp27", 6000, "Machine guns", {}, nil, TEAM_AMERICAN)
 
--- Equipment
+--Equipment
 AddItemToBUY("M1940 Stahlhelm", "Armor", "ent_armor_helmet1", 350, "Equipment", {}, nil, TEAM_GERMAN)
 AddItemToBUY("M1 Helmet", "Armor", "ent_armor_helmet7", 350, "Equipment", {}, nil, TEAM_AMERICAN)
 
--- Medical
-AddItemToBUY("Bandage", "Weapon", "weapon_bandage_sh", 200, "Medical", {})
-AddItemToBUY("Big Bandage", "Weapon", "weapon_bigbandage_sh", 400, "Medical", {})
-AddItemToBUY("Medkit", "Weapon", "weapon_medkit_sh", 650, "Medical", {})
-AddItemToBUY("Tourniquet", "Weapon", "weapon_tourniquet", 150, "Medical", {})
-AddItemToBUY("Painkillers", "Weapon", "weapon_painkillers", 200, "Medical", {})
+--Medical
 AddItemToBUY("Morphine", "Weapon", "weapon_morphine", 1000, "Medical", {})
 AddItemToBUY("Epipen", "Weapon", "weapon_adrenaline", 800, "Medical", {})
+AddItemToBUY("Medkit", "Weapon", "weapon_medkit_sh", 650, "Medical", {})
+AddItemToBUY("Big Bandage", "Weapon", "weapon_bigbandage_sh", 400, "Medical", {})
 AddItemToBUY("Bloodbag", "Weapon", "weapon_bloodbag", 400, "Medical", {})
 AddItemToBUY("Mannitol", "Weapon", "weapon_mannitol", 300, "Medical", {})
+AddItemToBUY("Bandage", "Weapon", "weapon_bandage_sh", 200, "Medical", {})
+AddItemToBUY("Painkillers", "Weapon", "weapon_painkillers", 200, "Medical", {})
+AddItemToBUY("Tourniquet", "Weapon", "weapon_tourniquet", 150, "Medical", {})
 AddItemToBUY("Decompression needle", "Weapon", "weapon_needle", 50, "Medical", {})
 
--- Grenades
-AddItemToBUY("Mk 2 Grenade", "Weapon", "weapon_hg_mk2_tpik", 450, "Grenades", {}, nil, TEAM_AMERICAN)
+--Grenades
 AddItemToBUY("F1 Grenade", "Weapon", "weapon_hg_f1_tpik", 500, "Grenades", {}, nil, TEAM_AMERICAN)
-AddItemToBUY("AN-M8 Smoke Grenade", "Weapon", "weapon_hg_m18_tpik", 350, "Grenades", {}, nil, TEAM_AMERICAN)
-AddItemToBUY("Stielhandgranate 24", "Weapon", "weapon_hg_mk2_tpik", 450, "Grenades", {}, nil, TEAM_GERMAN)
 AddItemToBUY("Eihandgranate 39", "Weapon", "weapon_hg_f1_tpik", 500, "Grenades", {}, nil, TEAM_GERMAN)
+AddItemToBUY("Mk 2 Grenade", "Weapon", "weapon_hg_mk2_tpik", 450, "Grenades", {}, nil, TEAM_AMERICAN)
+AddItemToBUY("Stielhandgranate 24", "Weapon", "weapon_hg_mk2_tpik", 450, "Grenades", {}, nil, TEAM_GERMAN)
+AddItemToBUY("AN-M8 Smoke Grenade", "Weapon", "weapon_hg_m18_tpik", 350, "Grenades", {}, nil, TEAM_AMERICAN)
 AddItemToBUY("M39 Nebelhandgranate", "Weapon", "weapon_hg_m18_tpik", 350, "Grenades", {}, nil, TEAM_GERMAN)
 
--- Melee
+--Melee
 AddItemToBUY("M1 Bayonet", "Weapon", "weapon_buck200knife", 300, "Melee", {}, nil, TEAM_AMERICAN)
 AddItemToBUY("AMES Entrenching Tool", "Weapon", "weapon_hg_crovel", 300, "Melee", {}, nil, TEAM_AMERICAN)
 AddItemToBUY("S84/98 III Bayonet", "Weapon", "weapon_sogknife", 300, "Melee", {}, nil, TEAM_GERMAN)
 AddItemToBUY("M1938 Klappspaten", "Weapon", "weapon_hg_crovel", 300, "Melee", {}, nil, TEAM_GERMAN)
 
 -- Ammo
-AddItemToBUY("7.65x17mm (30)", "Ammo", "ent_ammo_7.65x17mm", 50, "Ammo", {}, 30)
-AddItemToBUY("7.62x25mm (30)", "Ammo", "ent_ammo_7.62x25mm", 50, "Ammo", {}, 30)
-AddItemToBUY("9x19mm (30)", "Ammo", "ent_ammo_9x19mmparabellum", 75, "Ammo", {}, 30)
-AddItemToBUY(".45 ACP (30)", "Ammo", "ent_ammo_.45acp", 75, "Ammo", {}, 30)
-AddItemToBUY(".357 Magnum (20)", "Ammo", "ent_ammo_.357magnum", 75, "Ammo", {}, 20)
 AddItemToBUY("7.92x57mm (20)", "Ammo", "ent_ammo_7.92x57mmmauser", 100, "Ammo", {}, 20)
 AddItemToBUY("7.62x54mm (20)", "Ammo", "ent_ammo_7.62x54mm", 100, "Ammo", {}, 20)
 AddItemToBUY("7.62x51mm (20)", "Ammo", "ent_ammo_7.62x51mm", 100, "Ammo", {}, 20)
+AddItemToBUY("9x19mm (30)", "Ammo", "ent_ammo_9x19mmparabellum", 75, "Ammo", {}, 30)
+AddItemToBUY(".45 ACP (30)", "Ammo", "ent_ammo_.45acp", 75, "Ammo", {}, 30)
+AddItemToBUY(".357 Magnum (20)", "Ammo", "ent_ammo_.357magnum", 75, "Ammo", {}, 20)
+AddItemToBUY("7.65x17mm (30)", "Ammo", "ent_ammo_7.65x17mm", 50, "Ammo", {}, 30)
+AddItemToBUY("7.62x25mm (30)", "Ammo", "ent_ammo_7.62x25mm", 50, "Ammo", {}, 30)
--- gamemodes/zcity/gamemode/modes/tdm_ww2/sv_ww2.lua
+++ gamemodes/zcity/gamemode/modes/tdm_ww2/sv_ww2.lua
@@ -7,6 +7,7 @@ local teamData = {
     [TEAM_GERMAN] = {
         role = "German Forces",
         color = Color(120, 105, 85),
+
         models = {
             "models/wehrmacht_male01v1pm.mdl",
             "models/wehrmacht_male02v1pm.mdl",
@@ -18,6 +19,22 @@ local teamData = {
             "models/wehrmacht_male08v1pm.mdl",
             "models/wehrmacht_male09v1pm.mdl",
         },
+
+        bodygroups = {
+            [0] = 0, 
+            [1] = 0, 
+            [2] = 3, 
+            [3] = 0,
+            [4] = 0, 
+            [5] = 0, 
+            [6] = 0, 
+            [7] = 0,
+            [8] = 0, 
+            [9] = 0, 
+            [10] = 0, 
+            [11] = 0,
+        },
+
         radio = function()
             return math.Round(math.Rand(100, 108), 1)
         end,
@@ -26,6 +43,7 @@ local teamData = {
     [TEAM_AMERICAN] = {
         role = "American Forces",
         color = Color(70, 120, 85),
+
         models = {
             "models/normandyusmc1pm.mdl",
             "models/normandyusmc2pm.mdl",
@@ -37,37 +55,20 @@ local teamData = {
             "models/normandyusmc8pm.mdl",
             "models/normandyusmc9pm.mdl",
         },
+
+        bodygroups = {
+            [0] = 0, 
+            [1] = 0, 
+            [2] = 0,
+            [3] = 4, 
+            [4] = 0, 
+            [5] = 1,
+        },
+
         radio = function()
             return math.Round(math.Rand(88, 95), 1)
         end,
     },
-}
-
-local baseBodygroups = {
-    german_infantry = {
-
-        [0] = 0, -- wehrmacht_male01
-        [1] = 0, -- hands
-        [2] = 3, -- Helmet
-        [3] = 0, -- Coat
-        [4] = 0, -- goggles
-        [5] = 0, -- shemagh
-        [6] = 0, -- Grenades
-        [7] = 0, -- Uniforminsignia
-        [8] = 0, -- Scarf
-        [9] = 0, -- Gasmask
-        [10] = 0, -- Rank
-        [11] = 0, -- Gear
-    },
-
-    american_infantry = {
-        [0] = 0, -- soldier
-        [1] = 0, -- hand
-        [2] = 0, -- body
-        [3] = 4, -- Helmet
-        [4] = 0, -- Gear
-        [5] = 1, -- Beach_Gear
-    }
 }
 
 local modelWarnings = {}
@@ -82,42 +83,35 @@ local function GetTeamInfo(ply)
     return teamData[ply:Team()] or teamData[TEAM_GERMAN]
 end
 
-local function GetPlayerModel(ply, info)
-    local list = info.models
-    if not list or #list == 0 then return nil end
-
-    local seed = util.CRC(ply:SteamID64() or tostring(ply:EntIndex()))
-    local index = (tonumber(seed) % #list) + 1
-
-    return list[index]
+local function PickModel(info)
+    if not info or not info.models or #info.models == 0 then return nil end
+    return table.Random(info.models)
 end
 
-local function ApplyBodygroups(ply, model)
-    local groups
+local function ApplyBodygroups(ply, info)
+    if not info or not info.bodygroups then return end
 
-    if string.find(model, "wehrmacht") then
-        groups = baseBodygroups.german_infantry
-
-    elseif string.find(model, "usmc") or string.find(model, "american") then
-        groups = baseBodygroups.american_infantry
-    end
-
-    if not groups then return end
-
-    for id, value in pairs(groups) do
-        ply:SetBodygroup(id, value)
+    for k, v in pairs(info.bodygroups) do
+        if isnumber(k) and isnumber(v) then
+            ply:SetBodygroup(k, v)
+        end
     end
 end
 
 local function ApplyTeamModel(ply, info)
     if not IsValid(ply) or not info then return end
 
-    local model = GetPlayerModel(ply, info)
+    -- assign model once per life
+    if not ply.ZB_AssignedModel then
+        ply.ZB_AssignedModel = PickModel(info)
+    end
+
+    local model = ply.ZB_AssignedModel
     if not model then return end
 
     if util.IsValidModel and not util.IsValidModel(model) then
         if not modelWarnings[model] then
-            print("[WW2] Model invalid, forcing anyway: " .. model)
+            print("[WW2] Invalid model detected: " .. model)
             modelWarnings[model] = true
         end
     end
@@ -127,13 +121,12 @@ local function ApplyTeamModel(ply, info)
     end
 
     ply:SetModel(model)
-
-    ApplyBodygroups(ply, model)
-
     ply:SetPlayerColor(Vector(1, 1, 1))
     ply:SetNetVar("Accessories", "none")
     ply:SetSubMaterial()
     ply:SetSkin(0)
+
+    ApplyBodygroups(ply, info)
 end
 
 local function ApplyTeamModelDelayed(ply, info)
@@ -160,10 +153,20 @@ local function ApplyTeamModelDelayed(ply, info)
     end)
 end
 
+hook.Add("PlayerSpawn", "ZB_WW2_ApplyTeamModel", function(ply)
+    timer.Simple(0.25, function()
+        if not IsValid(ply) or not IsWW2Round() then return end
+        ply.ZB_AssignedModel = nil
+        ApplyTeamModelDelayed(ply, GetTeamInfo(ply))
+    end)
+end)
+
 function MODE:GiveEquipment()
     timer.Simple(0.1, function()
         for _, ply in player.Iterator() do
-            if not ply:Alive() then continue end
+            if not IsValid(ply) or not ply:Alive() then continue end
+
+            ply.ZB_AssignedModel = nil
 
             local inv = ply:GetNetVar("Inventory") or {}
             inv["Weapons"] = inv["Weapons"] or {}
@@ -177,6 +180,7 @@ function MODE:GiveEquipment()
 
             ply:SetPlayerClass()
             ApplyTeamModelDelayed(ply, info)
+
             zb.GiveRole(ply, info.role, info.color)
             ply:SetNetVar("CurPluv", "pluv")
 
@@ -186,7 +190,9 @@ function MODE:GiveEquipment()
             ply.organism.allowholster = true
 
             local Radio = ply:Give("weapon_walkie_talkie")
-            Radio.Frequency = info.radio()
+            if IsValid(Radio) then
+                Radio.Frequency = info.radio()
+            end
 
             ply:Give("weapon_hands_sh")
             ply:SelectWeapon("weapon_hands_sh")
@@ -201,31 +207,3 @@ function MODE:GiveEquipment()
         end
     end)
 end
-
-hook.Add("PlayerSpawn", "ZB_WW2_ApplyTeamModel", function(ply)
-    timer.Simple(0.25, function()
-        if not IsValid(ply) or not IsWW2Round() then return end
-        ApplyTeamModelDelayed(ply, GetTeamInfo(ply))
-    end)
-end)
-
-local nextModelCheck = 0
-hook.Add("Think", "ZB_WW2_KeepTeamModels", function()
-    if nextModelCheck > CurTime() then return end
-    nextModelCheck = CurTime() + 1
-
-    if not IsWW2Round() then return end
-
-    for _, ply in player.Iterator() do
-        if not ply:Alive() then continue end
-
-        local info = GetTeamInfo(ply)
-        local expectedModel = GetPlayerModel(ply, info)
-
-        if expectedModel and ply:GetModel() ~= expectedModel then
-            ApplyTeamModel(ply, info)
-        else
-            ApplyBodygroups(ply, expectedModel)
-        end
-    end
-end)
